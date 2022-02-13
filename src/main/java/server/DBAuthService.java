a9b878b63fbe18a20d253cf507b93e5dc7af52aa
danfilof <danila.anfilofyev@gmail.com>
2022-02-13 20:01:46 +0100

    DataBase filled with "loginX" , "passX" from 0 to 9. However there is no id = 1.

--- src/main/java/server/DBAuthService.java
+++ src/main/java/server/DBAuthService.java
@@ -1,9 +1,6 @@
 package server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DBAuthService implements AuthService {
     private Connection connection;
@@ -30,28 +27,35 @@ public class DBAuthService implements AuthService {
         DBAuthService dbAuthService = new DBAuthService();
         try {
             dbAuthService.connect();
-            dbAuthService.createTable();
-            dbAuthService.insert("login0", "pass0");
+            // Заполнил таблицу users login и pass, по аналогии с UserData из InMemoryAuthService
+           // dbAuthService.batchInsert();
         } finally {
             dbAuthService.disconnect();
         }
 
     }
 
-    private void createTable() {
-        try {
-            statement.executeUpdate("create table if not exists users (" + "id integer primary key autoincrement, " + "login text, " + "pass text" + ")");
+    private void batchInsert() {
+        try (PreparedStatement ps = connection.prepareStatement("insert into users (login, pass) values (?, ?)")) {
+            for (int i = 0; i < 10; i++) {
+                ps.setString(1, "login" + i);
+                ps.setString(2,"pass" + i);
+                ps.addBatch();
+            }
+            ps.executeBatch();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     private void insert(String login, String pass) {
-        try {
-           statement.executeUpdate("insert into Users (login, pass) values ('" + login + "', '" + pass + "')");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        try(PreparedStatement ps = connection.prepareStatement("insert into users (login, pass) values (?, ?)")) {
+            ps.setString(1, login);
+            ps.setString(2, pass);
+            ps.executeUpdate();
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
     }
 
 
