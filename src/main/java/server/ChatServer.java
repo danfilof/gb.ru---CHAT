8fbf80e633027505a2edcec68ead537f3b85e8b8
danfilof <danila.anfilofyev@gmail.com>
2022-02-24 21:13:30 +0100

    Technically homework completed

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class ChatServer {
@@ -11,6 +13,7 @@ public class ChatServer {
 
     private final Map<String, ClientHandler> clients;
     private final AuthService authService;
+    private ExecutorService executorService;
 
     public ChatServer() {
         this.clients = new HashMap<>();
@@ -18,15 +21,19 @@ public class ChatServer {
     }
 
     public void start() {
+        executorService = Executors.newCachedThreadPool();
         try(ServerSocket serverSocket = new ServerSocket(8189)) {
             while (true)  {
                 System.out.println("Waiting for connection...");
                 final Socket socket = serverSocket.accept();
-                new ClientHandler(socket, this);
+//                new ClientHandler(socket, this);
                 System.out.printf("The client %s has connected.", socket.getInetAddress().getHostName());
+                executorService.execute((Runnable) new ClientHandler(socket, this));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            executorService.shutdownNow();
         }
     }
 
